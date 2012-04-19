04643588bab2d12e44e197dbd5f485d7e91e4e66
Wille Faler <wille.faler@gmail.com>
2012-04-19 22:48:02 +0100

    logistic regression classifier done (more optimization algos to come though)

--- src/main/scala/org/scahal/classifier/logistic/LogisticRegressionClassifier.scala
+++ src/main/scala/org/scahal/classifier/logistic/LogisticRegressionClassifier.scala
@@ -1,10 +1,11 @@
 package org.scahal.classifier.logistic
 
+import org.scahal.math._
 import org.scahal.math.stats._
 import scalala.tensor.VectorCol
 import scalala.tensor.mutable._;
 import scalala.tensor.dense._
-import org.scahal.classifier.{ContinuousFeature, FeatureColumn, Event}
+import org.scahal.classifier.{Outcome, ContinuousFeature, FeatureColumn, Event}
 ;
 
 /**
@@ -18,18 +19,30 @@ import org.scahal.classifier.{ContinuousFeature, FeatureColumn, Event}
 
 object GradientAscent{
 
-  def apply(events: Seq[Event[Double]], alpha : Double = 0.001, maxCycles: Int = 500): VectorCol[Double] = {
-    val columns = events.foldLeft(Set[FeatureColumn]())((set, event) => event.features.map(_.featureColumn).toSet ++ set).toList
+  def apply(events: Seq[Event[Double]], alpha : Double = 0.001, maxCycles: Int = 500): List[Double] = {
+    val columns = events.foldLeft(Set[FeatureColumn]())((set, event) => event.features
+      .map(_.featureColumn).toSet ++ set).toList
     val labels: VectorCol[Double] = DenseVectorCol(events.map(_.outcome).toArray)
 
     val trainingSet: Matrix[Double] = DenseMatrix.tabulate(events.size, columns.size)((i,j) => {
-       events(i).features.find(columns(j) == _.featureColumn).map(_.asInstanceOf[ContinuousFeature].value.toDouble).getOrElse(0d)
+       events(i).features.find(columns(j) == _.featureColumn).
+         map(_.asInstanceOf[ContinuousFeature].value.toDouble).getOrElse(0d)
     })
 
     Range(0, maxCycles).foldLeft(DenseVectorCol.ones[Double](columns.size))((weights, c) => {
       val error = labels - ((trainingSet * weights).map(sigmoid(_)))
       weights + (trainingSet.t * error) * alpha
-    })
+    }).toList
   }
 
+}
+
+object LogisticRegressionClassifier{
+  def apply(features: Seq[ContinuousFeature], weights: List[Double]): List[Outcome[Double]] = {
+    val classification = Range(0, weights.size).foldLeft(0d)((in, index) =>{
+          in + features(index).value.toDouble * weights(index)
+        })
+    if(classification > 0.5) List(Outcome(1d, dec(100)))
+    else List(Outcome(0d, dec(100)))
+  }
 }
--- src/test/scala/org/scahal/classifier/logistic/LogisticRegressionSpec.scala
+++ src/test/scala/org/scahal/classifier/logistic/LogisticRegressionSpec.scala
@@ -18,16 +18,28 @@ class LogisticRegressionSpec extends Specification{ def is =
   "The Logistic Regression spec" ^
   p ^
     "Calculate the correct Gradient Ascent" !gradientAsc ^
+    "Classify with increasing accuracy using Gradient Ascent with more iterations" !increasingAccuracy ^
     end
 
   def gradientAsc = {
     val weights = GradientAscent(features)
-    (weights(0,0).apply(0) must be_==(4.124143489627892)) and
-    (weights(1,0).apply(0) must be_==(0.4800732928842445)) and
-    (weights(2,0).apply(0) must be_==(-0.6168481970344016))
+    (weights(0) must be_==(4.124143489627892)) and
+    (weights(1) must be_==(0.4800732928842445)) and
+    (weights(2) must be_==(-0.6168481970344016))
   }
 
-  def features = Source.fromInputStream(this.getClass.getResourceAsStream("/gradAscent.txt"), "UTF-8").getLines().map(line => {
+  def increasingAccuracy = {
+    (success(GradientAscent(features, maxCycles = 150)) must be_==(84)) and
+    (success(GradientAscent(features)) must be_==(91)) and
+    (success(GradientAscent(features, maxCycles = 10000)) must be_==(97))
+  }
+
+  def success(weights: List[Double]) = features.foldLeft(0d)((sum, event) => {
+        if(event.outcome == LogisticRegressionClassifier(event.features.map(_.asInstanceOf[ContinuousFeature]), weights)(0).value) sum + 1
+        else sum
+      })
+
+  lazy val features = Source.fromInputStream(this.getClass.getResourceAsStream("/gradAscent.txt"), "UTF-8").getLines().map(line => {
               val splits = line.split("\t")
               Event(splits(2).trim.toDouble, List(ContinuousFeature("0", 1d), ContinuousFeature("1",
                 splits(0).trim.toDouble), ContinuousFeature("2", splits(1).trim.toDouble)))
